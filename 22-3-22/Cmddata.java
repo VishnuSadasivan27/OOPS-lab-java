1c274c30dde067ead3f47bb5493564ebc313141d
VishnuSadasivan27 <98220243+VishnuSadasivan27@users.noreply.github.com>
2022-03-23 07:07:44 +0530

    Add files via upload

---
+++ 22-3-22/Cmddata.java
@@ -0,0 +1,11 @@
+public class Cmddata
+{
+	public static void main(String args[])
+	{
+		for(String a:args)
+		{
+			System.out.println("the entered details are :");
+			System.out.println("[%s]",&a);
+		}
+	}
+}
---
+++ 22-3-22/Program.java
@@ -0,0 +1,11 @@
+public class Program
+{
+	public static void main(String args[])
+	{
+		int i;
+		for(i = 0;i < args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
+	}
+}	
